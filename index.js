9db68d2957930ad367a141e0847a6bf046feb9ba
SrithinRaveendran <srithinraveendran@gmail.com>
2025-10-24 12:23:57 +0530

    cors link change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const sqlite3 = require('sqlite3')
 
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:3001" }));
+app.use(cors({ origin: "https://theaterbookingserver-3.onrender.com" }));
 const dbpath = path.join(__dirname, "database.db")
 
 let db = null
